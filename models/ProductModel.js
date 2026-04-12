fb66062f943af64ee78988bd05f8b1dd44a081dc
Anamika Mishra <anu812708@gmail.com>
2026-04-12 14:24:29 +0530

    backend initial

---
+++ models/ProductModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const productSchema = new mongoose.Schema({
+    productTitle: {
+        type: String,
+        required: true,
+    },
+    productCategory: {
+        type: String,
+        required: true,
+    },
+    productPrice: {
+        type: String,
+        required: true,
+    },
+
+    productDescription: {
+        type: String,
+        required: true,
+    },
+    
+    productImage: {
+        type: String,
+        required: true,
+    }
+
+})
+
+module.exports = mongoose.model("Product", productSchema)
